1c9ed7ecf9abe128b0bbc71b55588e1d313deac6
joaovperin <joaovperin@live.com>
2022-09-20 15:04:27 -0300

    changed from CharacterController to Transform movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,22 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class chara : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
-    private CharacterController controller;
     public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * speed * Time.deltaTime);
+        transform.position += move * speed * Time.deltaTime;
     }
 }
